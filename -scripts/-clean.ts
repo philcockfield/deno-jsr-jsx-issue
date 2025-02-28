5480286670b963cf99f3f0064a9b5e8a9f73501d
Phil Cockfield <phil@cockfield.net>
2025-02-28 14:53:56 +1300

    prep for publish

--- -scripts/-clean.ts
+++
@@ -1,5 +0,0 @@
-import { Fs } from "@sys/fs";
-
-await Fs.remove("./node_modules");
-await Fs.remove("./sample-vite/.vite");
-await Fs.remove("./sample-vite/dist");
--- sample-react/src/Foo.tsx
+++ sample-react/src/Foo.tsx
@@ -2,5 +2,15 @@ import { useState } from "react";
 
 export type FooProps = { msg?: string };
 export const Foo: React.FC<FooProps> = (props) => {
-  return <code>{`sample-react/Foo: ${msg}`}</code>;
+  const { msg = "🐷" } = props;
+
+  const [isOver, setOver] = useState(false);
+  const over = (isOver: boolean) => () => setOver(isOver);
+
+  return (
+    <code
+      onMouseEnter={over(true)}
+      onMouseLeave={over(false)}
+    >{`sample-react/Foo: ${msg} | isOver: ${String(isOver)}`}</code>
+  );
 };
